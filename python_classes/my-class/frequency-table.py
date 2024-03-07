7fce4c518337ba61d8322117d91ac266eb22f9e5
Yemsal4media <yemsalforlove2012@gmail.com>
2024-03-07 10:40:58 +0100

    ---------

--- python_classes/my-class/frequency-table.py
+++ python_classes/my-class/frequency-table.py
@@ -1,2 +1,18 @@
+'''
+Generate a frequency table for the ratings list (which is already initialized in the code editor).
+Start by creating an empty dictionary named content_ratings.
+Loop through the ratings list. For each iteration, complete the following:
+If the rating is already in content_ratings, then increment the frequency of that rating by 1.
+Else, initialize the rating with a value of 1 inside the content_ratings dictionary.
+Print content_ratings
+'''
 ratings = ['4+', '4+', '4+', '9+', '12+', '12+', '17+', '17+']
-def content_ratings:
+content_ratings = []
+
+for i in ratings:
+    if i in content_ratings:
+        i += 1
+    else:
+        i = 1
+print (content_ratings)
+print (ratings)
